a43d29b0f8e39d0d9c433b812580e189d0bb389b
sohanemon <sohanemon@outlook.com>
2026-07-15 18:03:21 +0600

    feat: update manifest feat(app): remove screenshot picker
    BREAKING CHANGE: removed screenshot picker functionality

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
-	console.log("Chrome extension installed");
+	console.log('Chrome extension template installed');
 });
 
 chrome.action.onClicked.addListener((tab) => {
-	console.log("Extension icon clicked", tab);
+	console.log('Chrome extension template icon clicked', tab);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,34 @@
-import tailwindcss from "@tailwindcss/vite";
-import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from '@tailwindcss/vite';
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		viteReact({
 			// @ts-expect-error https://react.dev/learn/react-compiler/installation#vite
 			babel: {
-				plugins: ["babel-plugin-react-compiler"],
+				plugins: ['babel-plugin-react-compiler'],
 			},
 		}),
 		tailwindcss(),
 		viteStaticCopy({
-			targets: [{ src: "public/manifest.json", dest: "." }],
+			targets: [{ src: 'public/manifest.json', dest: '.' }],
 		}),
 	],
 	build: {
-		outDir: "build",
+		outDir: 'build',
 		rollupOptions: {
 			input: {
-				main: "./index.html",
-				background: "./src/background.ts",
-				content: "./src/content.ts",
+				main: './index.html',
+				background: './src/background.ts',
+				content: './src/content.ts',
 			},
 			output: {
 				entryFileNames: (chunkInfo) => {
-					if (chunkInfo.name === "background") return "background.js";
-					if (chunkInfo.name === "content") return "content.js";
-					return "assets/[name]-[hash].js";
+					if (chunkInfo.name === 'background') return 'background.js';
+					if (chunkInfo.name === 'content') return 'content.js';
+					return 'assets/[name]-[hash].js';
 				},
 			},
 		},
