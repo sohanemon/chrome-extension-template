0552e484213bd76ba0d98188caca01520641d316
sohanemon <sohanemon@outlook.com>
2026-08-13 15:11:45 +0600

    feat: update icons and app layout
    - add button to app
    - update app styling
    - regenerate icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
 export default function App() {
-	return <main>Hi there!</main>;
+	return (
+		<main className="bg-background w-80 min-h-120 text-foreground p-2">
+			<h2 className="text-2xl font-bold">Hello, world!</h2>
+			<button
+				type="button"
+				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			>
+				Click me
+			</button>
+			<p>This is a small text</p>
+		</main>
+	);
 }
